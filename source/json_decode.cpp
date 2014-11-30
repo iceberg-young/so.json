3fa1d6b4a832557fa43935138548dc0d3f117628
Iceberg Young <iceberg.young@gmail.com>
2014-11-30 11:39:17 +0800

    Refactoring decode.

---
+++ source/json_decode.cpp
@@ -0,0 +1,226 @@
+#include <locale>
+#include <sstream>
+#include <tgmath.h>
+#include "json_data.hpp"
+#include "json_decode.hpp"
+
+namespace singularity {
+    using namespace json_uh;
+
+    json cascade(token t, cursor &i);
+
+    double parse_number(cursor &i);
+
+    std::string parse_string(cursor &i);
+
+    const std::locale c_locale{"C"};
+
+    // Cursor information for debug.
+    std::stringstream &operator<<(std::stringstream &ss, const cursor &i) {
+        ss << '[' << i.position() << ':' << *i << '(' << int(*i) << ")]";
+        return ss;
+    }
+
+    // Guess type of next node.
+    token next(cursor &i) {
+        while (std::isspace(*i, c_locale)) {++i;}
+        switch (char c = *i) {
+            case 'n': // null
+            case 'f': // false
+            case 't': // true
+            case '-': // number
+            case '"': // string
+            case '[': // array begin
+            case ']': // array end
+            case '{': // object begin
+            case '}': // object end
+            case ':': // name separator
+            case ',': // value separator
+                return static_cast<token>(c);
+
+            default:
+                if (std::isdigit(c, c_locale)) {
+                    return token::number;
+                }
+                else {
+                    std::stringstream ss;
+                    ss << i << " invalid value.";
+                    throw json_decode_error{ss.str()};
+                }
+        }
+    }
+
+    // Move cursor over the expected literals.
+    void pass_literals(cursor &i, const std::string &expected) {
+        for (auto c : expected) {
+            if (c != *++i) {
+                std::stringstream ss;
+                ss << i << " expected: " << c;
+                throw json_decode_error{ss.str()};
+            }
+        }
+    }
+
+    // Create a node start from the cursor.
+    json create(token t, cursor &i) {
+        switch (t) {
+            case token::null:
+                pass_literals(i, "ull");
+                return json{nullptr};
+
+            case token::boolean_false:
+                pass_literals(i, "alse");
+                return json{false};
+
+            case token::boolean_true:
+                pass_literals(i, "rue");
+                return json{true};
+
+            case token::number:
+                return json{parse_number(i)};
+
+            case token::string:
+                return json{parse_string(i)};
+
+            case token::array_begin:
+                return json{json::content_type::array};
+
+            case token::object_begin:
+                return json{json::content_type::object};
+
+            default: {
+                std::stringstream ss;
+                ss << i << " invalid node type.";
+                throw std::invalid_argument{ss.str()};
+            }
+        }
+    }
+
+    // Check value separator take post.
+    bool separated(token t, bool &s, const cursor &i) {
+        if (!s and t != token::value_separator) {
+            std::stringstream ss;
+            ss << i << " value separator (,) is expected.";
+            throw json_decode_error{ss.str()};
+        }
+        return !(s = !s);
+    }
+
+    void fill_array(json &array, cursor &i) {
+        token t;
+        bool s = true;
+        json::array_t &a = array;
+        while (token::array_end != (t = next(++i))) {
+            if (separated(t, s, i)) {
+                a.push_back(cascade(t, i));
+            }
+        }
+    }
+
+    void fill_object(json &object, cursor &i) {
+        token tn;
+        bool s = true;
+        json::object_t &o = object;
+        while (token::object_end != (tn = next(++i))) {
+            if (separated(tn, s, i)) {
+                // name
+                if (tn != token::string) {
+                    std::stringstream ss;
+                    ss << i << " name (string) is expected.";
+                    throw json_decode_error{ss.str()};
+                }
+                auto name = create(tn, i);
+
+                // separator (:)
+                auto ts = next(++i);
+                if (ts != token::name_separator) {
+                    std::stringstream ss;
+                    ss << i << " name separator (:) is expected.";
+                    throw json_decode_error{ss.str()};
+                }
+
+                // value
+                auto tv = next(++i);
+                auto value = cascade(tv, i);
+                o.insert(std::make_pair(name, value));
+            }
+        }
+    }
+
+    // Cascade create node.
+    json cascade(token t, cursor &i) {
+        json node = create(t, i);
+        switch (node.type()) {
+            case json::content_type::array:
+                fill_array(node, i);
+                break;
+
+            case json::content_type::object:
+                fill_object(node, i);
+                break;
+        }
+        ++i;
+        return node;
+    }
+
+    json json_uh::decode(const cursor &c) {
+        auto i = c;
+        auto token = next(i);
+        return cascade(token, i);
+    }
+
+    double parse_number(cursor &i) {
+        sci_t b = i;
+        // sign
+        if (*i == '-' or *i == '+') {++i;}
+        // integer
+        while (std::isdigit(*i)) {++i;}
+        // fraction
+        if (*i == '.') {
+            while (std::isdigit(*i)) {++i;}
+        }
+        // exponent
+        if (*i == 'e') {
+            if (*i == '-' or *i == '+') {++i;}
+            while (std::isdigit(*i)) {++i;}
+        }
+        return std::stod(std::string{b, i--});
+    }
+
+    std::string parse_string(cursor &i) {
+        std::stringstream ss;
+        while (*++i != '"') {
+            char c = *i;
+            if (c == '\\') {
+                switch (c = *++i) {
+                    case 'b':
+                        c = '\b';
+                        break;
+
+                    case 'f':
+                        c = '\f';
+                        break;
+
+                    case 'n':
+                        c = '\n';
+                        break;
+
+                    case 'r':
+                        c = '\r';
+                        break;
+
+                    case 't':
+                        c = '\t';
+                        break;
+
+                    case 'u':
+                        i += 4;
+                        // TODO
+                        continue;
+                }
+            }
+            ss << c;
+        }
+        return ss.str();
+    }
+}
---
+++ source/json_decode.hpp
@@ -0,0 +1,65 @@
+#ifndef INCLUDE_SINGULARITY_JSON_LEXER_ONCE_FLAG
+#define INCLUDE_SINGULARITY_JSON_LEXER_ONCE_FLAG
+
+#include "json.hpp"
+
+namespace singularity {
+    namespace json_uh {
+        using sci_t = std::string::const_iterator;
+
+        class cursor :
+          public sci_t
+        {
+        public:
+            cursor(sci_t &&begin, const sci_t &end) :
+              sci_t(begin),
+              begin(begin),
+              end(end) {
+            }
+
+        public:
+            cursor &operator++() {
+                if (*this == this->end) {
+                    throw std::out_of_range{"Already reached the end."};
+                }
+                sci_t::operator++();
+                return *this;
+            }
+
+            cursor &operator=(const cursor &other) {
+                *static_cast<sci_t *>(this) = other;
+                return *this;
+            }
+
+        public:
+            long position() const {
+                return *static_cast<const sci_t *>(this) - this->begin;
+            }
+
+        private:
+            const sci_t &begin;
+
+            const sci_t &end;
+        };
+
+        enum class token :
+          char
+        {
+            null = 'n',
+            boolean_false = 'f',
+            boolean_true = 't',
+            number = '-',
+            string = '"',
+            array_begin = '[',
+            array_end = ']',
+            object_begin = '{',
+            object_end = '}',
+            name_separator = ':',
+            value_separator = ',',
+        };
+
+        json decode(const cursor &c);
+    };
+}
+
+#endif//INCLUDE_SINGULARITY_JSON_LEXER_ONCE_FLAG
